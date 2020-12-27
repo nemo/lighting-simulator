7d3b340d38924459690ee98141f1aee67a469748
Nima Gardideh <nima@halfmoon.ws>
2020-12-26 20:11:49 -0500

    switching to CSV.

--- src/components/Evaluator/index.tsx
+++ src/components/Evaluator/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import Papa from 'papaparse'
 import './Evaluator.css'
 import Simulator from '../../library/Simulator'
 import Color from '../../library/Color'
+import example from '../../animations/example.csv'
 
 export default class Evaluator extends React.Component<any, any> {
   state = {
@@ -18,60 +20,60 @@ export default class Evaluator extends React.Component<any, any> {
 
       const grid = this.props.grid
       const simulator = new Simulator(grid)
-      const config = eval(`function parseJS() { return ${this.state.code}}; parseJS();`)
-      if (!config) {
-        throw new Error("You didn't return an object.")
-      }
+      const animation = Papa.parse(example)
 
-      if (!config) {
-        throw new Error("You didn't return an object.")
-      }
-
-      if (!config.sequence) {
-        throw new Error("You didn't include a sequence.")
-      }
-
-      const execute = (index : number) => {
-        if (index >= config.sequence.length) {
-          if (config.loop) { execute(0) }
-          return
-        }
-
-        const step = config.sequence[index]
-
-        console.log(`Executing Step #${index}.`)
-        if (!step) {
-          throw new Error(`Bad step [${index}]: NULL step.`)
-        }
-
-        if (!step.actions) {
-          throw new Error(`Bad step [${index}]: No actions provided.`)
-        }
-
-        // execute current step
-        step.actions.forEach((action : any, actionIndex : number) => {
-          if (!(action.x >= 0)) {
-            throw new Error(`Bad step [${index}]: Bad x coorindate in action at index [${actionIndex}].`)
-          }
-
-          if (!(action.y >= 0)) {
-            throw new Error(`Bad step [${index}]: Bad y coorindate in action at index [${actionIndex}].`)
-          }
-
-          if (!action.color) {
-            throw new Error(`Bad step [${index}]: No color in action at index [${actionIndex}].`)
-          }
-
-          console.log(`-> Action: ${actionIndex}. [${action.x}, ${action.y}] -> rgb(${action.color.r}, ${action.color.g}, ${action.color.b})`)
-          simulator.setLED(action.x, action.y, new Color(action.color.r, action.color.g, action.color.b))
-        })
-
-        // execute next step
-        this.setState({
-          timer: setTimeout(() => execute(index + 1), step.delta),
-          isRunning: true
-        })
-      }
+      console.log(animation)
+      // const config = eval(`function parseJS() { return ${this.state.code}}; parseJS();`)
+      //
+      // if (!config) {
+      //   throw new Error("You didn't return an object.")
+      // }
+      //
+      // if (!config.sequence) {
+      //   throw new Error("You didn't include a sequence.")
+      // }
+      //
+      // const execute = (index : number) => {
+      //   if (index >= config.sequence.length) {
+      //     if (config.loop) { execute(0) }
+      //     return
+      //   }
+      //
+      //   const step = config.sequence[index]
+      //
+      //   console.log(`Executing Step #${index}.`)
+      //   if (!step) {
+      //     throw new Error(`Bad step [${index}]: NULL step.`)
+      //   }
+      //
+      //   if (!step.actions) {
+      //     throw new Error(`Bad step [${index}]: No actions provided.`)
+      //   }
+      //
+      //   // execute current step
+      //   step.actions.forEach((action : any, actionIndex : number) => {
+      //     if (!(action.x >= 0)) {
+      //       throw new Error(`Bad step [${index}]: Bad x coorindate in action at index [${actionIndex}].`)
+      //     }
+      //
+      //     if (!(action.y >= 0)) {
+      //       throw new Error(`Bad step [${index}]: Bad y coorindate in action at index [${actionIndex}].`)
+      //     }
+      //
+      //     if (!action.color) {
+      //       throw new Error(`Bad step [${index}]: No color in action at index [${actionIndex}].`)
+      //     }
+      //
+      //     console.log(`-> Action: ${actionIndex}. [${action.x}, ${action.y}] -> rgb(${action.color.r}, ${action.color.g}, ${action.color.b})`)
+      //     simulator.setLED(action.x, action.y, new Color(action.color.r, action.color.g, action.color.b))
+      //   })
+      //
+      //   // execute next step
+      //   this.setState({
+      //     timer: setTimeout(() => execute(index + 1), step.delta),
+      //     isRunning: true
+      //   })
+      // }
 
       this.setState({
         isRunning: true
